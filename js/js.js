308cb9124ad558b0afc5db5b38f23791f4199e75
ritadelatorre <65496428+ritadelatorre@users.noreply.github.com>
2022-03-08 20:27:37 +0100

    Add files via upload

--- js/js.js
+++ js/js.js
@@ -1,1 +1,28 @@
+function desplegar(boton, parragrafo){
 
+   let quien = document.getElementById(boton).value;
+
+   if (quien=='-') {
+         document.getElementById(parragrafo).style.display = 'none';
+         document.getElementById(boton).value = '+';
+   } else {
+         document.getElementById(parragrafo).style.display = 'block';
+         document.getElementById(boton).value = '-';
+   }
+}
+
+function ocultarTodo(){
+      let numeroSecciones = document.getElementsByClassName("cuerpo").length;
+      
+      for (let i=0; i<numeroSecciones; i++) { 
+            document.getElementById("modulo" + i).style.display = 'none';
+            document.getElementById("b" + i).value = '+';
+	}
+}
+function mostrarTodo(){
+      let numeroSecciones = document.getElementsByClassName("cuerpo").length;
+      for (let i=0; i<numeroSecciones; i++) { 
+            document.getElementById("modulo" + i).style.display = 'block';
+            document.getElementById("b" + i).value = '-';
+	}
+}
